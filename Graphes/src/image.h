c473baa6acbf44e325b2abf18337e0f1e3716436
DACLIN CAMILLE p2210064 <camille.daclin@etu.univ-lyon1.fr>
2025-11-19 14:44:11 +0000

    image.h 0.2

--- Graphes/src/image.h
+++ Graphes/src/image.h
@@ -20,17 +20,37 @@ class Graph{
         Image *image;
         Pixel *pixel;
     public:
+<<<<<<< HEAD
+        //Constructeur - Destructeur
         Graph(Image, int, int);
         ~Graph();
 
+        //Fonctions d'accès
+        int get_global(Pixel *ligne, Pixel *colonne);
+        //Pour récupérer ses voisins:
+=======
+        Graph(Image, int, int);
+        ~Graph();
+
+>>>>>>> 5e5bebd8add02b3a9d758266a67628568ebca772
         int get_nord(int);
         int get_sud(int);
         int get_est(int);
         int get_ouest(int);
+<<<<<<< HEAD
+        //Propriétés pixel
+        
+
+
+        void afficher();
+        void sauvegarde();
+
+=======
 
         
 
 
+>>>>>>> 5e5bebd8add02b3a9d758266a67628568ebca772
 };
 
 #endif
