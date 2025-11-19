087c330c76d801f194329f8cfd5edeed04d7e34c
p2210064 <camille.daclin@etu.univ-lyon1.fr>
2025-11-19 17:00:50 +0100

    0.5

--- Graphes/src/image.cpp
+++ Graphes/src/image.cpp
@@ -1,14 +1,32 @@
 #include <iosteam>
+#include <cmath>
 #include "image.h"
 
-Image::Image
+Image::Image(const *string im){
+
+}
+
+Graph::Graph(Image im, float a, float b){
+    
+    image = new Image (im);
+    p = new pixel[im.hauteur * im.largeur];
+    for (int i=0, i< im.hauteur, i++){
+        for (int j = 0, j< im.largeur, j++){
+            
+        }
+    }
+}
+
 //Destructeur on enlève tout les pixels chargés dans le graphe
 Graph::~Graph(){
     delete pixel;
 }
 
-int Graph::get_global(){
-    
+int Graph::get_valuation(pixel *p, pixel *q){
+    float a,b;
+    a = pow(p->capacite - q->capacite,2);
+    b = 2 * pow(p->ndg,2);
+    return a + b;   
 }
 
 int Graph::get_nord(int i){
@@ -33,4 +51,8 @@ int Graph::get_ouest(int i){
     if ((i+image->largeur) % image->largeur == 0)
         return -1
     return i-1;
+}
+
+void Graph::afficher(){
+
 }
--- Graphes/src/image.h
+++ Graphes/src/image.h
@@ -20,37 +20,23 @@ class Graph{
         Image *image;
         Pixel *pixel;
     public:
-<<<<<<< HEAD
         //Constructeur - Destructeur
-        Graph(Image, int, int);
+        Graph(Image im, float i, float j);
         ~Graph();
 
-        //Fonctions d'accès
-        int get_global(Pixel *ligne, Pixel *colonne);
+        //Fonctions pour avoir la capacité des arcs vers ses voisins
+        int get_valuation(pixel *ligne, pixel *colonne);
         //Pour récupérer ses voisins:
-=======
-        Graph(Image, int, int);
-        ~Graph();
-
->>>>>>> 5e5bebd8add02b3a9d758266a67628568ebca772
         int get_nord(int);
         int get_sud(int);
         int get_est(int);
         int get_ouest(int);
-<<<<<<< HEAD
-        //Propriétés pixel
         
 
 
         void afficher();
         void sauvegarde();
 
-=======
-
-        
-
-
->>>>>>> 5e5bebd8add02b3a9d758266a67628568ebca772
 };
 
 #endif
