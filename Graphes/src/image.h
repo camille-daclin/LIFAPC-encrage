1b9215f5248bc08456d3f4e9be5520d1cfd4af80
DACLIN CAMILLE p2210064 <camille.daclin@etu.univ-lyon1.fr>
2025-11-19 14:05:39 +0000

    Upload New File

---
+++ Graphes/src/image.h
@@ -0,0 +1,24 @@
+#ifndef IMAGE_
+#define IMAGE_
+
+using namespace std;
+
+class Image{
+    
+    private:
+        int *pixels;
+        int hauteur;
+        int largeur;
+
+    public:
+
+};
+
+class Graph{
+    private:
+        Image *image;
+        Pixel *pixel;
+    public:
+};
+
+#endif
