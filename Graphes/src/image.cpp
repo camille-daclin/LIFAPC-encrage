89e082a9e7ba73f12aed78838ef734801cc0e47d
DACLIN CAMILLE p2210064 <camille.daclin@etu.univ-lyon1.fr>
2025-11-19 15:06:04 +0000

    Image.cpp 0.1

--- Graphes/src/image.cpp
+++ Graphes/src/image.cpp
@@ -1,1 +1,36 @@
+#include <iosteam>
 #include "image.h"
+
+Image::Image
+//Destructeur on enlève tout les pixels chargés dans le graphe
+Graph::~Graph(){
+    delete pixel;
+}
+
+int Graph::get_global(){
+    
+}
+
+int Graph::get_nord(int i){
+    if (i < image->largeur)
+        return -1;
+    return i - image->largeur;
+}
+
+int Graph::get_sud(int i){
+    if (i>image->largeur *(image->hauteur-1)-1)
+        return -1;
+    return i+image->largeur;
+}
+
+int Graph::get_est(int i){
+    if (((i+1) % image->largeur)==0)
+        return -1;
+    return i+1;
+}
+
+int Graph::get_ouest(int i){
+    if ((i+image->largeur) % image->largeur == 0)
+        return -1
+    return i-1;
+}
